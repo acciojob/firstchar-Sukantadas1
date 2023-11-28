f2c4ff91728bc8326d4a088beaf8afac8bd45d3a
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-28 10:38:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstChar(text) {
-  // your code here
+ // Remove leading and trailing spaces
+  const trimmedStr = text.trim();
+
+  // Check if the trimmed string is empty
+  if (trimmedStr === '') {
+    return '';
+  }
+
+  // Return the first character of the trimmed string
+  return trimmedStr.charAt(0);
 }
 
 // Do not change the code below
